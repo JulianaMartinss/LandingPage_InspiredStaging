930c6611d6d938f62d9a999b8ab53b1351b741fe
Juliana Martins <julianaalvesmartins1@gmail.com>
2025-11-29 04:35:51 -0300

    Deploy: add build files to docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LandingPage_InspiredStaging/', // ⚠ Coloque exatamente o nome da sua repo
   plugins: [react()],
-  server: {
-    host: true
-  }
 })
